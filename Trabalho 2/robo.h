2c8ed32398ad202c7923ac0e5d8b99302d8242fe
Felipe Chabatura Neto <felipechabat@gmail.com>
2017-12-14 20:52:05 -0200

    Update robo.h

--- Trabalho 2/robo.h
+++ Trabalho 2/robo.h
@@ -3,34 +3,36 @@
 #define ROBO_H
 
 #include <stdio.h>
+#include <cstdlib>
 #include <string.h>
 #include <math.h>
 #include <GL/glut.h>
 
-#define MAXLAB 30
-#define SPEED 0.10;
-#define INF 112345
-#define CAMTURN 0.11111111111111 * 2
-#define ROBOTURN 10
-#define CENTRO MAXLAB / 2
+#define MAXLAB 30 // Tamanho máximo do labirinto
+#define SPEED 0.10; //Velocidade do Robô
+#define INF 112345 //Valor de infinito
+#define CAMTURN 0.11111111111111 * 2 //Constante para movimento da câmera
+#define ROBOTURN 10 //Constante para movimento do Robô
+#define CENTRO MAXLAB / 2 //Centro do labirinto
 
+//Estrutura de ponto no labirinto (local do grid)
 typedef struct{
   int x, y;
   double speedx, speedy;
   int nextdir;
 }point_t;
 
-void Inicializa(void);
-GLUquadricObj *quadric(void);
-void getViewPos(unsigned char key, int x, int y);
-void desenhaCubo(int posi, int posj);
-void desenhaCuboRobo();
-void desenhaRobo();
-void desenhaParede(void);
-void desenhaObjetos(void);
-void desenha(void);
-void Redesenha(int);
-void AlteraTamanhoJanela(GLsizei w, GLsizei h);
-void loadTexture(const char * imagepath, int i);
+void Inicializa(void); //Função com rotinas de inicialização
+GLUquadricObj *quadric(void); //Função para desenhar quádricas
+void getViewPos(unsigned char key, int x, int y); //Função para tratar comandos do teclado
+void desenhaCubo(int posi, int posj); //Função que desenha cubo para as paredes
+void desenhaCuboRobo(); //Função que desenha os cubos do robo
+void desenhaRobo(); //Função que desenha o robo
+void desenhaParede(void); //Função para desenhar as paredes do labirinto
+void desenhaObjetos(void); //Função que desenha os demais objetos no labirinto
+void desenha(void); //Função que desenha tudo
+void Redesenha(int); //Função de redesenho
+void AlteraTamanhoJanela(GLsizei w, GLsizei h); //Função para alterar tamanho da janela
+void loadTexture(const char * imagepath, int i); //Função que lẽ os arquivos de textura bmp e os carrega no open gl
 
 #endif
